83c9f7b054c956fd4745ff2cb5122b7e6d44b3c1
Psiuk1741 <gamen1741@gmail.com>
2023-01-31 23:41:33 +0200

    Done hw1

---
+++ lesson-1/script.js
@@ -0,0 +1,61 @@
+// 1
+console.log('TASK 1');
+
+let a = 'hello';
+console.log(a);
+
+let b = 'owu';
+console.log(b);
+
+let c = 'com';
+console.log(c);
+
+let d = 'ua';
+console.log(d);
+
+let e = 1;
+console.log(e);
+
+let f = 10;
+console.log(f);
+
+let g = -999;
+console.log(g);
+
+let h = 123;
+console.log(h);
+
+let i = 3.14;
+console.log(i);
+
+let j = 2.7;
+console.log(j);
+
+let k = 16;
+console.log(k);
+
+let l = true;
+console.log(l);
+
+let m = false;
+console.log(m);
+
+// 2
+console.log('TASK 2');
+let firstName = 'Dmytro', middleName = 'Yuriyovych', lastName = 'Psiuk';
+console.log(`${firstName} ${middleName} ${lastName}`);
+
+// 3
+console.log('TASK 3');
+let a1 = 100; let b1 = '100'; let c1 = true;
+console.log(typeof a1, a1);
+console.log(typeof b1, b1);
+console.log(typeof c1, c1);
+
+// 4
+console.log('TASK 4');
+let name = prompt('Як Вас звати?','');
+let pobatkovi = prompt('Як Вас по-Батькові?','');
+let year = prompt('Введіть рік народження','');
+
+console.log(`${name} ${pobatkovi} ${year}`);
