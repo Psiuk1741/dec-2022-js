2b35ce9bcdf3a4b6d778d530be5b82b2bed9ee31
Psiuk1741 <gamen1741@gmail.com>
2023-02-01 11:17:06 +0200

    Fixed bug`s hw1

--- lesson-1/script.js
+++ lesson-1/script.js
@@ -1,4 +1,5 @@
-// 1
+// 1 Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+// Вивести кожну змінну за допомогою: console.log
 console.log('TASK 1');
 
 let a = 'hello';
@@ -40,22 +41,26 @@ console.log(l);
 let m = false;
 console.log(m);
 
-// 2
+// 2 Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
 console.log('TASK 2');
 let firstName = 'Dmytro', middleName = 'Yuriyovych', lastName = 'Psiuk';
 console.log(`${firstName} ${middleName} ${lastName}`);
 
-// 3
+// 3 За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.   let a = 100; let b = '100'; let c = true;
 console.log('TASK 3');
-let a1 = 100; let b1 = '100'; let c1 = true;
+let a1 = 100;
+let b1 = '100';
+let c1 = true;
 console.log(typeof a1, a1);
 console.log(typeof b1, b1);
 console.log(typeof c1, c1);
 
-// 4
+// 4 За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
 console.log('TASK 4');
-let name = prompt('Як Вас звати?','');
-let pobatkovi = prompt('Як Вас по-Батькові?','');
-let year = prompt('Введіть рік народження','');
+let name = prompt('Як Вас звати?', '');
+let pobatkovi = prompt('Як Вас по-Батькові?', '');
+let year = prompt('Введіть рік народження', '0');
+year = parseInt(year);
+
 
 console.log(`${name} ${pobatkovi} ${year}`);
